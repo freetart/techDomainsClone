427aa8c44447f2969a0d7bc9332e9a06ee540af0
habib <habibdev96@gmail.com>
2021-03-28 09:57:57 -0600

    created news section on home page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// carousels
+const storiesCarousel = {
+  type: "carousel",
+  autoplay: 8000,
+  hoverpause: true,
+};
+new Glide(".glide1", storiesCarousel).mount();
+
+const spotlightCarousel = {
+  type: "carousel",
+  autoplay: 4000,
+  hoverpause: true,
+  perView: 3,
+};
+new Glide(".glide2", spotlightCarousel).mount();
+
+const newsCarousel = {
+  type: "carousel",
+  autoplay: 2000,
+  perView: 4,
+};
+new Glide(".glide3", newsCarousel).mount();
