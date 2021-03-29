8b39e7aec0d5e03bb0a96fea79d6a3bb9f70c86c
habib <habibdev96@gmail.com>
2021-03-28 18:59:49 -0600

    created hamburger menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,28 @@
 "use strict";
 
+// hamburger menu
+const menuIcon = document.querySelector(".hamburger-menu");
+const menuNavbar = document.querySelector(".menu-navbar");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+
+menuIcon.addEventListener("click", () => {
+  menuNavbar.classList.toggle("change");
+  if (menuNavbar.classList.contains("change")) {
+    overlay.classList.remove("hidden");
+    body.style.overflowY = "hidden";
+  } else {
+    overlay.classList.add("hidden");
+    body.style.overflowY = "auto";
+  }
+});
+
+overlay.addEventListener("click", () => {
+  menuNavbar.classList.remove("change");
+  overlay.classList.add("hidden");
+  body.style.overflowY = "auto";
+});
+
 // carousels
 const width = window.innerWidth;
 
