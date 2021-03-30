bdd2dd20dacf1e283c81de098e0f5bba3abe4b77
habib <habibdev96@gmail.com>
2021-03-29 18:05:16 -0600

    created typewriter effect on hero input

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,6 +23,53 @@ overlay.addEventListener("click", () => {
   body.style.overflowY = "auto";
 });
 
+// hero search
+const textDisplay = document.querySelector(".hero-input");
+const phrases = ["habibdev.tech", "traversymedia.tech", "codehawke.tech"];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+  textDisplay.placeholder = currentPhrase.join("");
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.placeholder = currentPhrase.join("");
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.placeholder = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+  setTimeout(loop, time);
+}
+loop();
+
 // carousels
 const width = window.innerWidth;
 
